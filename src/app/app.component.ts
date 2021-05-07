6991839f406d39d021ca34b2d903858fa6966c45
Sean Blessing <snblessing@gmail.com>
2021-05-06 20:16:30 -0400

    catchup commit post class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'friends-list-jbc202008';
+  newFriend = '';
+  isDisabled = false;
+  // create an array named friends and populate with some friends
+  friends = ["Sean", "Ron", "Julie", "Phil"];
+
+  addFriend() {
+    this.friends.push(this.newFriend);
+  }
 }
